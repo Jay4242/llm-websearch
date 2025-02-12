32f767373778e56450829f8ecc7199dfe9c7e320
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2025-02-12 08:06:08 -0800

    Update llm-websearch.bash
    Stop using /tmp/, I forgot to change some to /dev/shm/ instead.  Now it should all be pointed to the same temp directory.

--- llm-websearch.bash
+++ llm-websearch.bash
@@ -51,11 +51,11 @@ for link in "${links[@]}" ; do
       else
          #Else simply try to download the URL.
          #Save the webpage to a temp file and clean up the HTML tags.
-         curl -s -L -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' "${lurl}" | html2text > /tmp/llm-websearch.txt
+         curl -s -L -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' "${lurl}" | html2text > /dev/shm/llm-websearch.txt
       fi
 
       #Ask the LLM to check the text for helpful information and summarize it.
-      pans=$(llm-python-file.py /tmp/llm-websearch.txt "You are a helpful research assistant." "We are trying to research \`${sterm}\` and we have a webpage with the url \`${lurl}\` that has the following text:" "Summarize the portions of the page that help answer \`${sterm}\`.  Only directly summarize the text to help the research and nothing else." "0.7" | sed -e 's/\\n/ /g' -e "s/*//g" -e 's/\\//g' | tr -d '\n')
+      pans=$(llm-python-file.py /dev/shm/llm-websearch.txt "You are a helpful research assistant." "We are trying to research \`${sterm}\` and we have a webpage with the url \`${lurl}\` that has the following text:" "Summarize the portions of the page that help answer \`${sterm}\`.  Only directly summarize the text to help the research and nothing else." "0.7" | sed -e 's/\\n/ /g' -e "s/*//g" -e 's/\\//g' | tr -d '\n')
 
        #Error checking the output for page errors like 403.  Commented out because it was being too aggressive, we'll let the last bot filter things until we work that out.
 #      echeck=$(llm-python-chat.py "You are an error checking assistant.  You check if the text represents a webpage error or not."  "\`\`\`${pans}\`\`\`\\nOutput only \`PASS\` if the text appears to not be an error, or \`FAIL\` if the text appears to be an error message." "0.0" | sed -e 's/.*content="//g' -e "s/.*content='//g" -e 's/"\, role=.*//g' -e "s/', role=.*//g" -e 's/\\n/ /g' -e "s/*//g" -e 's/\\//g' )
@@ -74,16 +74,16 @@ for link in "${links[@]}" ; do
 done
 
 #Clear out the old temp file.
-echo -n "" > /tmp/llm-websearch.txt
+echo -n "" > /dev/shm/llm-websearch.txt
 
 #Start looping through the array we created of site info and summaries.
 for dan in "${dans[@]}" ; do
 
    #Echo the website and LLM summary to the temp file.
-   echo "${dan}"  >> /tmp/llm-websearch.txt
+   echo "${dan}"  >> /dev/shm/llm-websearch.txt
 
 done
 
 #Read the temp file and try to summarize what we've collected in total.  Suggest one URL over all to visit.
-llm-python-file.py /tmp/llm-websearch.txt "You are a helpful research assistant." "We are trying to research \`${sterm}\` and we have compiled the following list of URLs and their contents." "Try to answer \`${sterm}\` using information from the text.  Provide a total summary of the combined information followed by what you think is the BEST URL source from the list." "0.7" | sed -e 's/\\n/ /g' -e "s/*//g" -e 's/\\//g'
+llm-python-file.py /dev/shm/llm-websearch.txt "You are a helpful research assistant." "We are trying to research \`${sterm}\` and we have compiled the following list of URLs and their contents." "Try to answer \`${sterm}\` using information from the text.  Provide a total summary of the combined information followed by what you think is the BEST URL source from the list." "0.7" | sed -e 's/\\n/ /g' -e "s/*//g" -e 's/\\//g'
 
